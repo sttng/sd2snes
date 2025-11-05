30250b21069f3f48545901831896330a600df1e0
sttng <46765778+sttng@users.noreply.github.com>
2025-11-06 04:05:43 +0800

    Add files via upload

--- src/smc.c
+++ src/smc.c
@@ -260,8 +260,8 @@ void smc_id(snes_romprops_t* props, uint32_t file_offset) {
       if(header->carttype == 0xf5 || header->carttype == 0xf9) {
         props->has_spc7110 = 1;
         props->fpga_conf = FPGA_SPC7110;
-        #props->error = MENU_ERR_NOIMPL;
-        #props->error_param = (uint8_t*)"SPC7110";
+        /* props->error = MENU_ERR_NOIMPL; */
+        /* props->error_param = (uint8_t*)"SPC7110"; */
       }
       break;
 
